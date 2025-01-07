dec4f601d1be966898ce462b0d2d010b7bb1fa1c
Syed Muhammad Ali Hassan <syedmuhammadalihassan2002@hotmail.com>
2025-01-07 02:54:57 +0000

    initialized the backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,res) => {
+    req.send("signup route");
+};
+
+export const login = (req,res) => {
+    req.send("login route");
+};
+
+export const logout = (req,res) => {
+    req.send("logout route");
+};
